cdec54e4ca035594b5bd848fdfbd8ac05c926c82
Dieudonné César <cesar.dieudonne@etudiant.univ-reims.fr>
2020-10-04 18:04:11 +0200

    correction

--- Programmation/classes/LieuMission.py
+++ Programmation/classes/LieuMission.py
@@ -2,7 +2,7 @@ from classes.Lieu import Lieu
 
 class LieuMission(Lieu):
     def __init__(self, stock = []):
-        super().__init__('R')
+        super().__init__('M')
         self.stock = stock
 
     def getSigne(self):
